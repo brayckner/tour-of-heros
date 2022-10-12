7ab8339175aea883b3f78221d717e4197579b8a7
Brayckner <brayckner@gmail.com>
2022-10-12 14:37:27 -0400

    Making a heros list, not finished yet

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+    {id: 12, name: 'Dr. Nice'},
+    {id: 13, name: 'Maverick'},
+    {id: 14, name: 'Mr. Incredible'},
+    {id: 15, name: 'Invincible'},
+    {id: 16, name: 'Iron Man'},
+    {id: 17, name: 'Dr. Banner'},
+    {id: 18, name: 'El Chapulin Colorado'},
+    {id: 19, name: 'StarKiller'},
+    {id: 20, name: 'Anikin Skywalker'}
+];
